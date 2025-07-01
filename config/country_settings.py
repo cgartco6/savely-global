4fe800c64268da0b177267ad56e6ad51337feb10
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-07-01 20:16:44 +0200

    Create country_settings.py

---
+++ config/country_settings.py
@@ -0,0 +1,38 @@
+COUNTRIES = [
+    "Germany", "France", "UK", "Japan", "USA", 
+    "Australia", "Ireland", "Spain", "Portugal", 
+    "Italy", "Denmark", "Sweden", "Norway", 
+    "Finland", "South Africa", "New Zealand"
+]
+
+CURRENCY_MAP = {
+    "Germany": "EUR",
+    "France": "EUR",
+    "UK": "GBP",
+    "Japan": "JPY",
+    "USA": "USD",
+    "Australia": "AUD",
+    "Ireland": "EUR",
+    "Spain": "EUR",
+    "Portugal": "EUR",
+    "Italy": "EUR",
+    "Denmark": "DKK",
+    "Sweden": "SEK",
+    "Norway": "NOK",
+    "Finland": "EUR",
+    "South Africa": "ZAR",
+    "New Zealand": "NZD"
+}
+
+PRICE_MAP = {
+    "EUR": 8.99,
+    "GBP": 7.99,
+    "JPY": 1100,
+    "USD": 9.99,
+    "AUD": 14.99,
+    "DKK": 67.99,
+    "SEK": 99.99,
+    "NOK": 99.99,
+    "ZAR": 149.99,
+    "NZD": 16.99
+}
